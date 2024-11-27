c9ad3cf0645c7b53452300a266491c0f1c7813a3
Yeshwant Rudra <rudrayeshwant1411@gmail.com>
2024-11-27 15:11:00 +0530

    Update route_table.tf

--- route_table.tf
+++ route_table.tf
@@ -1,3 +1,14 @@
+resource "aws_route_table" "yt_route_table_public_subnet" {
+  vpc_id = aws_vpc.custom_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw_vpc.id
+  }
+  tags = {
+    Name = "Public subnet Route Table"
+  }
+}
+
 resource "aws_route_table_association" "public_subnet_association" {
   route_table_id = aws_route_table.yt_route_table_public_subnet.id
   count          = length(var.vpc_availability_zones)
