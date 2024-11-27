50b2a5bb8840e98bc99704a584c1195fc29d3f37
Yeshwant Rudra <rudrayeshwant1411@gmail.com>
2024-11-27 15:21:28 +0530

    Update cloud_watch_scale_up.tf

--- cloud_watch_scale_up.tf
+++ cloud_watch_scale_up.tf
@@ -14,3 +14,13 @@ resource "aws_cloudwatch_metric_alarm" "cpu_high_alarm" {
 
   alarm_actions = [aws_autoscaling_policy.scale_up.arn]
 }
+
+resource "aws_autoscaling_policy" "scale_up" {
+  name                   = "scale-up-policy"
+  scaling_adjustment      = 1  # Number of instances to add
+  adjustment_type         = "ChangeInCapacity"
+  cooldown                = 300  # 5 minutes cooldown
+  autoscaling_group_name   = aws_autoscaling_group.ec2_asg.name  # Correct argument
+
+  policy_type             = "SimpleScaling"
+}
