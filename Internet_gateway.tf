4d46d8a3557852a1f5d26e03cbf6f7d63ce8e67c
Yeshwant Rudra <rudrayeshwant1411@gmail.com>
2024-11-27 15:09:54 +0530

    Update Internet_gateway.tf

--- Internet_gateway.tf
+++ Internet_gateway.tf
@@ -4,15 +4,3 @@ resource "aws_internet_gateway" "igw_vpc" {
     Name = "YT-Internet Gateway"
   }
 }
-
-//4. Route table for public subnet
-resource "aws_route_table" "yt_route_table_public_subnet" {
-  vpc_id = aws_vpc.custom_vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw_vpc.id
-  }
-  tags = {
-    Name = "Public subnet Route Table"
-  }
-}
