03e95dbf28ae683440d5d44c925d64d6868efc92
Yeshwant Rudra <rudrayeshwant1411@gmail.com>
2024-11-27 12:52:07 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,58 @@
+resource "aws_launch_template" "ec2_launch_template" {
+  name = "yt-web-server"
+  image_id      = "ami-0dee22c13ea7a9a67" # Copy the ami id from AWS console
+  instance_type = "t2.micro"
+
+  network_interfaces {
+    associate_public_ip_address = false
+    security_groups             = [aws_security_group.ec2_sg.id]
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "yt-ec2-web-server"
+    }
+  }
+}
+
+resource "aws_autoscaling_group" "ec2_asg" {
+  name                = "yt-web-server-asg"
+  desired_capacity    = 2
+  min_size            = 2
+  max_size            = 3
+  target_group_arns   = [aws_lb_target_group.alb_ec2_tg.arn]
+  vpc_zone_identifier = aws_subnet.private_subnet[*].id
+
+  launch_template {
+    id      = aws_launch_template.ec2_launch_template.id
+    version = "$Latest"
+  }
+
+  health_check_type = "EC2"
+}
+
+resource "aws_autoscaling_policy" "scale_up" {
+  name                   = "scale-up-policy"
+  scaling_adjustment      = 1
+  adjustment_type         = "ChangeInCapacity"
+  cooldown                = 300
+  autoscaling_group_name   = aws_autoscaling_group.ec2_asg.name
+  policy_type             = "SimpleScaling"
+}
+
+resource "aws_autoscaling_policy" "scale_down" {
+  name                   = "scale-down-policy"
+  scaling_adjustment      = -1
+  adjustment_type         = "ChangeInCapacity"
+  cooldown                = 300
+  autoscaling_group_name   = aws_autoscaling_group.ec2_asg.name
+  policy_type             = "SimpleScaling"
+}
+
+resource "aws_cloudwatch_metric_alarm" "cpu_high_alarm" {
+  alarm_name          = "cpu-usage-high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "CPUUtilization"
+  namespace           = "
