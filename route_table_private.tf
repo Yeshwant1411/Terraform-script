8ccc5543e54ae746e7aa047002a27b1ba02a6cb3
Yeshwant Rudra <rudrayeshwant1411@gmail.com>
2024-11-27 15:12:45 +0530

    Update route_table_private.tf

--- route_table_private.tf
+++ route_table_private.tf
@@ -9,3 +9,9 @@ resource "aws_route_table" "yt_route_table_private_subnet" {
     Name = "Private subnet Route Table"
   }
 }
+
+resource "aws_route_table_association" "private_subnet_association" {
+  route_table_id = aws_route_table.yt_route_table_private_subnet.id
+  count          = length(var.vpc_availability_zones)
+  subnet_id      = element(aws_subnet.private_subnet[*].id, count.index)
+}
